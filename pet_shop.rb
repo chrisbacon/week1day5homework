0f998ade520e2e461e7aeb81799c4407afa230b0
chrisbacon <bacon.chris@gmail.com>
2016-11-04 14:32:19 +0000

    ninth function written and tested

--- pet_shop.rb
+++ pet_shop.rb
@@ -35,4 +35,11 @@ def find_pet_by_name(pet_shop, name)
     return pet if pet[:name] == name
   end
   return nil 
-end 
+end
+
+def remove_pet_by_name(pet_shop, name)
+  for pet in pet_shop[:pets]
+    pet_shops[:pets].delete(pet) if pet[:name] == name
+  end
+end
+
