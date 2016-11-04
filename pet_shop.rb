a37c57f5cc3ea3e862fc3a8e33f9141c289f7a08
chrisbacon <bacon.chris@gmail.com>
2016-11-04 14:53:52 +0000

    eleventh function written and tested

--- pet_shop.rb
+++ pet_shop.rb
@@ -51,6 +51,6 @@ def customer_pet_count(customer)
   return customer[:pets].count
 end
 
-# def add_pet_to_customer(customer, new_pet)
-#   customer[:pets].push(new_pet)
-# end
+def add_pet_to_customer(customer, new_pet)
+  customer[:pets].push(new_pet)
+end
